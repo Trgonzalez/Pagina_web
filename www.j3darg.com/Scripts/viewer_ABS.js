58ffd1cf2955922a31ff37add783b5c26dd0e34f
Tristan Gonzalez Branca <trgonzalez@itba.edu.ar>
2023-09-27 20:50:21 -0300

    post

--- www.j3darg.com/Scripts/viewer_ABS.js
+++ www.j3darg.com/Scripts/viewer_ABS.js
@@ -426,6 +426,11 @@ function prevSend() {
     else
         $('#confirm-modal').modal('open');
 }
+// manda post a netlify
+function submitForm() {
+    var form = document.querySelector('form[name="my-form"]');
+    form.submit();
+  }
 
 /**Envía todo al siguiente paso */
 function send() {
